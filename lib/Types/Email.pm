d046367b416cfb5f82994f53bc998a19a773a2cb
Erik Carlsson <info@code301.com>
2015-03-06 17:33:19 +0100

    Dev

--- lib/Types/Email.pm
+++ lib/Types/Email.pm
@@ -1,4 +1,4 @@
-use 5.10.1;
+use 5.008001;
 use strict;
 use warnings;
 
@@ -7,8 +7,41 @@ package Types::Email;
 # VERSION
 # ABSTRACT: Short intro
 
+use Type::Library -base, -declare qw/EmailAddress EmailMessage EmailAddresses EmailMessages/;
+use Type::Utils;
+use Types::Standard qw/Object Str ArrayRef/;
+
+use Email::Valid;
+use Email::Abstract;
+
+declare EmailAddress,
+    as Str,
+    where { Email::Valid->address($_) },
+    message { 'Must be a valid email address' };
+
+declare EmailAddresses,
+    as ArrayRef[EmailAddress],
+    message { 'Must be an arrayref of valid email addresses' };
+
+coerce EmailAddresses,
+    from Str,
+    via { [ $_ ] };
 
 
+declare EmailMessage,
+    as Object,
+    where { Email::Abstract->new($_) },
+    message { 'Must be something Email::Abstract recognizes' };
+
+declare EmailMessages,
+    as ArrayRef[Object],
+    where { not grep { not Email::Abstract->new($_) } @$_ },
+    message { 'Must be an arrayref of something Email::Abstract recognizes' };
+
+coerce EmailMessage,
+    as Object,
+    via { Email::Abstract->new($_) };
+    
 1;
 
 
