56311c59d829153eedbf2637165fd2d8c09d433b
Erik Carlsson <info@code301.com>
2015-03-06 17:09:26 +0100

    Init

---
+++ lib/Types/Email.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Types::Email;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Types::Email;
+
+=head1 DESCRIPTION
+
+Types::Email is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+use Types::Email;
+
+fail('this test is TODO!');
+done_testing;
